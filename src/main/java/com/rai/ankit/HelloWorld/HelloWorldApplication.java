626fa5aec2c9a67f1400453a1d2a274e46c3c840
Ankit Rai <ankit.rai@live.in>
2024-05-13 17:29:57 +0100

    variable

--- src/main/java/com/rai/ankit/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/rai/ankit/HelloWorld/HelloWorldApplication.java
@@ -8,7 +8,7 @@ public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
-		System.out.println("ankit");
+		System.out.println("ankit1");
 	}
 
 }
